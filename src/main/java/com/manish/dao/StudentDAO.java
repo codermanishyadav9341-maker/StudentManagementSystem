4e2f5e389b1bd4004de81e520d50eecccb0304ba
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-02 16:02:21 +0530

    Initial commit

---
+++ src/main/java/com/manish/dao/StudentDAO.java
@@ -0,0 +1,28 @@
+package com.manish.dao;
+
+import com.manish.model.Student;
+import java.util.List;
+public interface StudentDAO {
+
+    //Add Student
+    boolean addStudent(Student student);
+
+    //View All Student
+    List<Student> getAllStudents();
+
+    // Student search by id;
+    Student getStudentById(int id);
+
+    //Search Student by name;
+    List<Student> getStudentsByName(String name);
+
+    //Update Student;
+    boolean updateStudent(Student student);
+
+    //Delete Student;
+    boolean deleteStudent(int id);
+
+
+    // check Student exists;
+    boolean existsStudent(int id);
+}
