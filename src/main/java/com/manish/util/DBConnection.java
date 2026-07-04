541b77d6e5e468b01c566c5e05358ef7771a788d
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-04 15:05:34 +0530

    Initial commit

---
+++ src/main/java/com/manish/util/DBConnection.java
@@ -0,0 +1,36 @@
+package com.manish.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/student_management_System";
+
+    private static final String USERNAME = "root";
+
+    private static final String PASSWORD = "982105";
+
+    private DBConnection(){  // Prevent object creation;
+
+    }
+
+    public static Connection getConnection(){
+
+        Connection connection = null;
+
+        try{
+
+            connection = DriverManager.getConnection(
+                    URL,
+                    USERNAME,
+                    PASSWORD
+            );
+        }
+        catch(SQLException e){
+           System.out.println("Database connection failed");
+        }
+        return connection;
+    }
+}
