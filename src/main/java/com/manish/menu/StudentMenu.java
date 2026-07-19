c4ef67e30c09539a50e60d08d93b0748fd419d70
Manish_Yadav_08 <codermanishyadav9341@gmail.com>
2026-07-19 10:41:57 +0530

    Initial commit

--- src/main/java/com/manish/menu/StudentMenu.java
+++ src/main/java/com/manish/menu/StudentMenu.java
@@ -2,8 +2,13 @@ package com.manish.menu;
 
 import com.manish.service.StudentService;
 import com.manish.service.StudentServiceImpl;
+import com.manish.dao.StudentDAO;
+import com.manish.dao.StudentDAOImpl;
+import com.manish.service.StudentService;
+import com.manish.service.StudentServiceImpl;
 
 import java.util.Scanner;
+
 public class StudentMenu {
 
     private final Scanner scan = new Scanner(System.in);
@@ -29,6 +34,48 @@ public class StudentMenu {
 
             //Take user choice
             System.out.print("Enter Choice:- ");
+            int choice = scan.nextInt();
+
+            //Consume newLine
+            scan.nextLine();
+
+            switch(choice){
+
+                case 1:
+                    addStudent();  //Add Student;
+                    break;
+
+                case 2:
+                    viewAllStudents();
+                    break;
+
+                case 3:
+                    searchById();
+                    break;
+
+                case 4:
+                    searchByName();
+                    break;
+
+                case 5:
+                    searchByEmail();
+                    break;
+
+                case 6:
+                    updateStudent();
+                    break;
+
+                case 7:
+                    deleteStudent();
+                    break;
+
+                case 8:
+                    System.out.println("Thank You");
+                    System.exit(0);
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
         }
     }
 }
